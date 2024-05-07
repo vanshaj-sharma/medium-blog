2c528adf230bf4be5c3fdf53f4cfab9cb710aad6
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-05-07 10:50:48 +0530

    added prisma&db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,49 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+  } catch (error) {}
+  // return c.text("sign up route");
+});
+
+app.post("/api/v1/signin", (c) => {
+  return c.text("sign in route");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("blog 'post' route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("blog 'put' route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("blog 'get' route");
+});
+
+export default app;
