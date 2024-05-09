03505e03cc9b11ce7024de4d1b7ab10a4d51fd4e
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-05-09 11:19:37 +0530

    initialzed forntend and made quote page

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,10 @@
+import { Quote } from "../components/Quote";
+
+export const Signup = () => {
+  return (
+    <div className="grid grid-cols-2">
+      <div></div>
+      <Quote />
+    </div>
+  );
+};
