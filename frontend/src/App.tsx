5b7116ecddc3dae564f209d54ee527be7839cff0
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-05-11 23:52:59 +0530

    fixing issues 3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/subdirectory">
+      <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
