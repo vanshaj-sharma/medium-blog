dfffe2032508dee95490b1932b9627067302becc
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-05-11 23:42:34 +0530

    adding vercel adjustments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/subdirectory">
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
